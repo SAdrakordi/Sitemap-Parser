aa6fa578ca3dadb133dcca0180fc5a23e680609f
Satop <90753704+SAdrakordi@users.noreply.github.com>
2022-10-24 10:08:22 +0330

    Update XML Parser.py

--- XML Parser.py
+++ XML Parser.py
@@ -1,3 +1,5 @@
 site_map = xmltodict.parse(requests.get('--your sitemap url--').text)
 urls = [url['loc'] for url in site_map['urlset']['url']]
-print("\n".join(urls))
+return urls
+#If you want to see the urls uncomment the next line.
+#print("\n".join(urls))
